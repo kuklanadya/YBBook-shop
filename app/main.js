1103acd4e6b2e1e9353a2ccedf5f658fe2d087b1
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-20 02:07:37 +0200

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 import ControllerCards from "./cards/controller_cards.js";
 import ControllerSort from "./sort/controller_sort.js";
+import ControllerFilter from "./filter/controller_filter.js";
 
 const controllerCards = new ControllerCards();
-const controllerSort = new ControllerSort(controllerCards.handleSort.bind(controllerCards));
+const controllerSort = new ControllerSort();
+const controllerFilter = new ControllerFilter();

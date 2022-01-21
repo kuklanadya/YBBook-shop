7ce2e4601f3a42bf61416d843b0ea44aab0973b8
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-21 15:08:17 +0200

    Delete app directory

--- app/main.js
+++
@@ -1,7 +0,0 @@
-import ControllerCards from "./cards/controller_cards.js";
-import ControllerSort from "./sort/controller_sort.js";
-import ControllerFilter from "./filter/controller_filter.js";
-
-const controllerCards = new ControllerCards();
-const controllerSort = new ControllerSort();
-const controllerFilter = new ControllerFilter();

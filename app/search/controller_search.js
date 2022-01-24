d9aaae2028f0300f87c848591b216de68a872589
Naira Kazeian <nkazeian@gmail.com>
2022-01-24 22:28:05 +0200

    плавный выезд акардиона+добавлено preventDefault

--- app/search/controller_search.js
+++ app/search/controller_search.js
@@ -2,13 +2,14 @@ import Publisher from "../publisher.js";
 import ViewSearch from "./view_search.js";
 
 export default class ControllerSearch {
-   constructor() {
-      this.view = new ViewSearch(this.onSearch);
-      this.view.init();
-      this.publisher = new Publisher();
-   }
-   onSearch = (event) => {
-      this.searchQuery = event.target.value;
-      this.publisher.notify("ON_SEARCH", this.searchQuery);
-   };
+  constructor() {
+    this.view = new ViewSearch(this.onSearch);
+    this.view.init();
+    this.publisher = new Publisher();
+  }
+  onSearch = (event) => {
+    event.preventDefault();
+    this.searchQuery = event.target.value;
+    this.publisher.notify("ON_SEARCH", this.searchQuery);
+  };
 }

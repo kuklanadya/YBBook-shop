12d381c9f544f26af7cd3b4295c569517ec8c3e4
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-24 03:56:10 +0200

    Update main.js

--- app/main.js
+++ app/main.js
@@ -3,9 +3,11 @@ import ControllerSort from "./sort/controller_sort.js";
 import ControllerFilter from "./filter/controller_filter.js";
 import ControllerModal from "./modal/controller_modal.js";
 import ControllerSearch from "./search/controller_search.js";
+import ControllerInfo from "./info/controller_info.js";
 
 const controllerCards = new ControllerCards();
-const controllerSort = new ControllerSort();
-const controllerFilter = new ControllerFilter();
-const controllerModal = new ControllerModal();
 const controllerSearch = new ControllerSearch();
+const controllerFilter = new ControllerFilter();
+const controllerSort = new ControllerSort();
+const controllerModal = new ControllerModal();
+const controllerInfo = new ControllerInfo();

622732347bf3022c1755efc237eed6ace8e97415
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-25 20:31:41 +0200

    Update model_cards.js

--- app/cards/model_cards.js
+++ app/cards/model_cards.js
@@ -1,6 +1,7 @@
 export default class ModelCards {
    URL_SHEET =
       "https://docs.google.com/spreadsheets/d/e/2PACX-1vQirlHBLU299aaZ7rqDdrC48Cb50Tb-8s_YK7-XIIpoFAS9uy9pnDki0uuxehglgHYLID9xt7q4wWRg/pub?output=tsv";
+   goods = [];
 
    getData() {
       return fetch(this.URL_SHEET)
@@ -34,15 +35,6 @@ export default class ModelCards {
       return this.data;
    }
 
-   addGenresAccordion() {
-      const header = document.querySelector(".accordion_header");
-      const content = header.nextElementSibling;
-      header.addEventListener('click', () => {
-         header.classList.toggle('active');
-         content.classList.toggle('active');
-      });
-   }
-
    filterData(filterType) {
       this.data = [];
       for (const type of filterType) {
@@ -74,4 +66,33 @@ export default class ModelCards {
       }
       return getBooks;
    }
+
+   findGoods = (ids) => {
+      let goods = [];
+      for (const id of ids) {
+         let i = 0;
+         goods = goods.concat(this.data.filter(card => card.id === id));
+         if (!goods[i].quantity) {
+            goods[i].quantity = 0;
+         }
+         goods[i].quantity += 1;
+         i++;
+      }
+      this.goods = Array.from(new Set(this.goods.concat(goods)));
+      return goods;
+   }
+
+   changeQuantity(event) {
+      const item = event.target.closest('.cart-modal-item');
+      const id = item.dataset.id;
+      const itemObj = this.goods.find(item => item.id === id);
+      if (event.target.classList.contains('cart-modal-plus')) {
+         itemObj.quantity += 1;
+      }
+      if (event.target.classList.contains('cart-modal-minus')) {
+         itemObj.quantity -= 1;
+      }
+      return this.goods;
+   }
 }
+

dd9d25ad662f5558a69e9aa0a28082adb23576c6
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-25 20:31:33 +0200

    Update view_cards.js

--- app/cards/view_cards.js
+++ app/cards/view_cards.js
@@ -2,10 +2,25 @@ export default class ViewCards {
    BODY_MAIN = document.body.querySelector("main");
    BODY_HEADER = document.body.querySelector("header");
 
+   constructor(handleAddToCart, handleChangeQuantity) {
+      this.handleAddToCart = handleAddToCart;
+      this.handleChangeQuantity = handleChangeQuantity;
+   }
+
+   addToCartListener = () => {
+      [...document.querySelectorAll(".buy")]
+         .forEach(btn => btn.addEventListener('click', this.handleAddToCart));
+
+   }
+
+   addChangeQuantityListener = () => {
+      [...document.querySelectorAll(".cart-modal-add")]
+         .forEach(btn => btn.addEventListener('click', this.handleChangeQuantity));
+   }
+
    renderCards(cards) {
       this.BODY_MAIN.innerHTML = "";
-      this.BODY_MAIN.insertAdjacentHTML("beforeend", cards.map(this.getBookHTML).join("")
-      );
+      this.BODY_MAIN.insertAdjacentHTML("beforeend", cards.map(this.getBookHTML).join(""));
    }
 
    renderModalCard({ id, title, author, image, price, description, genre, year, rating }) {
@@ -15,15 +30,14 @@ export default class ViewCards {
         <img src="${image}" class="card-img" alt="${title}"/>
         <span class="close"></span>
           <div class="card-body">
-            <h5 class="card-title">${title}</h5>
-            <!--book name -->
-            <p class="card-text">Автор: ${author}</p>
+            <p class="card-title">${title}</p>
+            <p class="card-text">${author}</p>
             <div class="card-text card-desc">Описание: ${description}</div>
             <p class="card-text">Год публикации: ${year}</p>
             <p class="card-text">Жанр: ${genre}</p>
             <p class="card-text">Рейтинг: ${rating}</p>
             <p class="card-text">Цена: <span>${price}₴</span></p>
-            <a href="#" class="btn btn-primary">Купить</a>
+            <a href="#" class="btn buy">Добавить в корзину</a>
           </div>
          </div>
       </div>`;
@@ -44,15 +58,32 @@ export default class ViewCards {
       document.querySelector(".backdrop").remove();
    }
 
+   addGenresAccordion() {
+      const header = document.querySelector(".accordion_header");
+      const content = header.nextElementSibling;
+      header.addEventListener('click', () => {
+         header.classList.toggle('active');
+         content.classList.toggle('active');
+      });
+   }
+
    renderHeader = () => {
       const header = `
-      <div>
-         <h2>YBBooks</h2>
+      <div class="interface">
+         <h2 class="interface-title">YBBooks</h2>
+         <div class="interface-icons">
+            <div class="interface-history">
+               <a><img src="/img/history.svg" alt="history"></a>
+            </div>
+            <div class="interface-cart">
+            <a><img src="/img/cart.svg" alt="history"></a>
+            </div>
+         </div>
       </div>`;
       this.BODY_HEADER.insertAdjacentHTML("afterbegin", header);
    }
 
-   getBookHTML({ id, title, author, image, price, description, genre, year, rating }) {
+   getBookHTML({ id, title, author, image, price, genre, rating }) {
       return `
       <div class="card" data-id="${id}">
          <div class="card-head">

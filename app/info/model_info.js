1a971d5c5caf18af9565a80fe35da4ac93a6ba8f
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-26 02:37:01 +0200

    Update model_info.js

--- app/info/model_info.js
+++ app/info/model_info.js
@@ -1,24 +1,42 @@
 export default class ModelInfo {
    TG_BASE_URL = 'https://api.telegram.org/bot5246638907:AAEORSmJTXhY1q1Nxwmz41k5qqgxiFyLBn4/sendMessage?';
    CHAT_ID = 663180905;
-
    goods = [];
 
    sendMsg = text => {
-      const url = `${this.TG_BASE_URL}chat_id=${this.CHAT_ID}&text=${text}\n Спасибо за Ваш заказ! `;
-      //fetch(url);
+      const url = `${this.TG_BASE_URL}chat_id=${this.CHAT_ID}&text=
+      Вы только что оформили заказ в интернет-магазине YBBooks.%0AСодержимое заказа:%0A ${text}
+      %0AСпасибо за то, что выбрали нас! Наш оператор свяжется с вами в ближайшее время.`;
+      fetch(url);
    }
 
-   //updateCart = id => {
-      // if (!this.goods[id]) {
-      //    this.goods[id] = 0;
-      // }
-      // this.goods[id] += 1;
-      // this.goods[sum] += 1;
-   //}
+   sendInfo = (goods) => {
+      this.goods = goods;
+      let text = '';
+      for (const item of goods) {
+         text += `"${item.title}. ${item.author}" в количестве ${item.quantity},%0A`;
+      }
+      text = text.slice(0, -4) + '.';
+      this.sendMsg(text);
+   }
 
-   sendInfo = _ => {
-      const message = JSON.stringify(this.goods);
-      this.sendMsg(message);
+   validateInput = () => {
+      let result = true;
+      const name_reg = '/([\u0401\u0451\u0410-\u044fa-z]+ ?)+/gi'; 
+      const phone_reg = '/\+380(\d{2})[-\. ]?(\d{3})[-\. ]?(\d{2})[-\. ]?(\d{2})[-\. ]?/g';
+      const email_reg = '/([a-z0-9]+\.?[a-z0-9])+@[a-z]+\.[a-z0-9]+/gi';
+      if (!document.querySelector('.form-modal-name').value.match(name_reg)) {
+         document.querySelector('.form-modal-name').classList.add('invalid');
+         result = false;
+      }
+      if (!document.querySelector('.form-modal-phone').value.match(phone_reg)) {
+         document.querySelector('.form-modal-phone').classList.add('invalid');
+         result = false;
+      }
+      if (!document.querySelector('.form-modal-email').value.match(email_reg)) {
+         document.querySelector('.form-modal-email').classList.add('invalid');
+         result = false;
+      }
+      return result;
    }
 }

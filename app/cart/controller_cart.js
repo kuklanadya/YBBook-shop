71db577af839abece44395f7594af6bf1767c074
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-26 15:32:02 +0200

    Update controller_cart.js

--- app/cart/controller_cart.js
+++ app/cart/controller_cart.js
@@ -1,26 +1,23 @@
 import Publisher from "../publisher.js";
+import ModelCart from "./model_cart.js";
 import ViewCart from "./view_cart.js";
 
 export default class ControllerCart {
-   goods = [];
    constructor() {
       this.publisher = new Publisher();
       this.view = new ViewCart(this.publisher);
+      this.model = new ModelCart();
 
       this.view.init();
-      this.publisher.subscribe('FIND_GOODS', this.getGoods);
+      this.publisher.subscribe('FIND_GOODS', this.model.getGoods);
       this.publisher.subscribe('ON_RENDER_CART', this.renderGoods);
       this.publisher.subscribe('CHANGE_GOODS', this.renderGoods);
    }
 
-   getGoods = (item) => {
-      this.goods = Array.from(new Set(this.goods.concat(item)));
-   }
-
    renderGoods = () => {
-      this.goods = this.goods.filter(item => item.quantity > 0)
-      this.view.isCartEmpty(this.goods);
-      this.view.renderGoods(this.goods);
-      this.publisher.notify("ON_RENDER_GOODS");
+      this.model.filterGoods();
+      this.view.isCartEmpty(this.model.goods);
+      this.view.renderGoods(this.model.goods);
+      this.publisher.notify("ON_RENDER_GOODS", this.model.goods);
    }
 }

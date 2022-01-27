d57bb09d029e28f28487dc3e4dabb65315c9c176
kuklanadya <92209829+kuklanadya@users.noreply.github.com>
2022-01-28 01:39:20 +0200

    Delete app directory

--- app/history/model_history.js
+++
@@ -1,8 +0,0 @@
-export default class ModelHistory {
-   goods = [];
-
-   getDataFromStorage() {
-      this.goods = this.goods.concat(JSON.parse(localStorage.getItem('order') ?? 0));
-      this.goods = this.goods.filter(el => el !== 0);
-   }
-}
